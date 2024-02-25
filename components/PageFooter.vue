b4612b2354a8fc5840e4c921072eeb9a79d2f541
fan <291950304@qq.com>
2024-02-26 00:02:53 +0800

    chore: 页脚组件

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-    尾部信息
+    <div class="mt-auto bg-gray-900 text-gray-100 text-center">
+        <div class="container">
+            <div class="py-3">
+                使用 nuxt3 + ts + vite + tailwindcss 的练手项目，接口由 <a class="text-[#6EE7B7]" href="http://nuxt3.dishawang.com/">帝莎编程</a> 提供
+            </div>
+            <div class="border-t border-gray-700 text-xs py-3">
+                Copyright © 2023 fanjs.cn
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
