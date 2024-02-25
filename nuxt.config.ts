2316ac64e763e8254d20819f7d9ca90c863365c8
fan <291950304@qq.com>
2024-02-26 00:01:12 +0800

    chore: 添加vueuse/motion动画库

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,9 @@ import Components from "unplugin-vue-components/vite"
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
 
 export default defineNuxtConfig({
+    // 使用 devtools
     devtools: { enabled: true },
+    // tailwindcss 支持
     css: ["~/assets/css/main.css"],
     postcss: {
         plugins: {
@@ -12,6 +14,8 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    // 导入第三方模块
+    modules: ["@vueuse/motion/nuxt"],
     build: {
         transpile:
             process.env.NODE_ENV === "production" ? ["naive-ui", "vueuc", "@css-render/vue3-ssr", "juggle/resize-observer"] : ["@juggle/resize-observer"],
