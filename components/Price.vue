362080477bb28304a576214b885d5b2faf7f1e42
fan <291950304@qq.com>
2024-02-26 15:52:23 +0800

    chore: 价格组件

---
+++ components/Price.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+    value: {
+        type: [String, Number],
+        default: 0,
+    },
+    through: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <span :class="through ? 'text-gray-500 text-xs line-through' : 'text-rose-500 font-bold'">
+        {{ Number.parseFloat(value) === 0 ? '免费' : `￥${value}` }}
+    </span>
+</template>
+
+<style scoped>
+
+</style>
