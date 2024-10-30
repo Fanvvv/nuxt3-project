8468095744e9a04ff5586176aad6a08877e02c56
fan <291950304@qq.com>
2024-10-30 10:22:10 +0800

    chore: empty data component

---
+++ components/Empty.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+    desc: {
+        type: String,
+        default: "暂无数据",
+    },
+})
+</script>
+
+<template>
+    <n-empty :description="desc" size="large" class="py-10" />
+</template>
+
+<style scoped>
+
+</style>
