c36c4cc5f7e15194301b1dffb831a76eed54b464
fan <291950304@qq.com>
2024-10-30 10:29:05 +0800

    chore: 搜索页

---
+++ pages/search/[type]/[page].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+const title = ref(route.query.keyword as string)
+// const type = ref(route.params.type)
+
+useHead({ title })
+definePageMeta({
+    middleware: ["search"],
+})
+</script>
+
+<template>
+    <div>search page</div>
+</template>
+
+<style scoped>
+
+</style>
