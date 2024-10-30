35cc14acd0e37b5c90772e394e1c2cb6f5edca0e
fan <291950304@qq.com>
2024-10-30 10:26:26 +0800

    chore: 骨架屏组件

---
+++ components/Fan/Skeleton.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps({
+    bg: {
+        type: String,
+        default: "#efefef",
+    },
+    width: {
+        type: String,
+        default: "100px",
+    },
+    height: {
+        type: String,
+        default: "100px",
+    },
+    animated: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <div
+        class="inline-block relative overflow-hidden align-middle animate-[fade_1s_linear_infinite_alternate]"
+        :style="{ width, height }"
+        :class="{ blink: animated }"
+    >
+        <div class="w-full h-full rounded-[2px]" :style="{ backgroundColor: bg }" />
+    </div>
+</template>
+
+<style scoped>
+.blink::after {
+  content: "";
+  background: linear-gradient(
+    to left,
+    rgba(255,255,255,0) 0,
+    rgba(255,255,255,0.3) 50%,
+    rgba(255,255,255,0)100%
+  );
+  @apply absolute animate-[blink_1s_ease_0s_infinite] top-0 w-1/2 h-full skew-x-[-45]
+}
+
+@keyframes blink {
+  0% {
+    left: -100%;
+  }
+  100% {
+    left: 120px;
+  }
+}
+@keyframes fade {
+  from {
+    opacity: 0.2;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
---
+++ components/Loading/Skeleton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <n-card v-for="i in 3" :key="i" class="mb-5">
+        <fan-skeleton bg="#e4e4e4" width="306px" height="306px" animated />
+        <fan-skeleton bg="#e4e4e4" width="160px" height="24px" animated />
+        <fan-skeleton bg="#e4e4e4" width="120px" height="24px" animated />
+    </n-card>
+</template>
+
+<style scoped>
+
+</style>
