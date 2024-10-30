caa7830661c18384c2b22cdbce577aef72e89b17
fan <291950304@qq.com>
2024-10-30 10:23:58 +0800

    chore: tab component

---
+++ components/Ui/Tab.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full flex overflow-x-auto bg-white mb-4 rounded">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/Ui/TabItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <span
+        class="py-3 px-4 text-gray-700 text-sm cursor-pointer flex-shrink-0 hover:text-blue-400"
+        :class="{ 'tab-item-active': active }"
+    >
+        <slot />
+    </span>
+</template>
+
+<style scoped>
+.tab-item-active {
+  border-bottom: 2px solid;
+  @apply text-blue-400 border-blue-400;
+}
+</style>
