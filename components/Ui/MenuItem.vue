ae8e3a987213e5f3b3174dba2c050c95cb312eda
fan <291950304@qq.com>
2024-10-30 10:26:40 +0800

    chore: 菜单组件

---
+++ components/Ui/MenuItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<template>
+    <div
+        class="px-2 py-1 mx-1 rounded cursor-pointer hover:bg-blue-50 hover:text-blue-700 active:!bg-amber-100 transition-all duration-200"
+        :class="{ 'menu-item-active': active }"
+    >
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.menu-item-active {
+  @apply !bg-blue-100 !text-blue-600;
+}
+</style>
