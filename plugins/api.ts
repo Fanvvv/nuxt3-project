325cc97328b75b409d5ab35a8ebf2dc65dfc9565
fan <291950304@qq.com>
2024-10-30 10:21:50 +0800

    chore: api plugin

--- plugins/api.ts
+++ plugins/api.ts
@@ -9,22 +9,28 @@ interface IApiInstance {
 }
 
 export default defineNuxtPlugin(() => {
+    // 获取 nuxt.config.ts 中的配置
     const config = useRuntimeConfig()
-
     const fetchOptions: FetchOptions = {
         baseURL: config.public.apiBaseUrl as string,
         headers: {
             appid: config.public.appid as string,
         },
     }
+    // 用户登录，默认传 token
+    const token = useCookie("token")
+    // 解决 HeadersInit 类型中没有 token 值，ts 报错的问题
+    const headers = fetchOptions?.headers ? new Headers(fetchOptions.headers) : new Headers()
+    if (token.value && !headers.has("token"))
+        headers.set("token", token.value)
 
-    const apiFecther = $fetch.create(fetchOptions)
+    fetchOptions.headers = headers
+    const apiFetcher = $fetch.create(fetchOptions)
 
     const modules: IApiInstance = {
-        users: new UsersModule(apiFecther),
-        index: new IndexModule(apiFecther),
+        users: new UsersModule(apiFetcher),
+        index: new IndexModule(apiFetcher),
     }
-
     return {
         provide: {
             api: modules,
